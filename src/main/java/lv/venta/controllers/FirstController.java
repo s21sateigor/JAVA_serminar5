636752bf4e8ff4e53017fa66b1872f998972bd99
localuser <localuser@VEA.LV>
2023-04-20 16:01:58 +0300

    New: second controller with model

--- src/main/java/lv/venta/controllers/FirstController.java
+++ src/main/java/lv/venta/controllers/FirstController.java
@@ -1,6 +1,9 @@
 package lv.venta.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,5 +15,13 @@ public class FirstController {
 		System.out.println("Sveiki!");
 		return "hello-page";//there will be hello-page.html
 	}
+	
+	//TODO create controller with model and add String in the model
+	@GetMapping("/msg") //localhost:8080/msg
+	public String getMsgFunc(Model model) {
+		model.addAttribute("packet", "Message from Karina");
+		return "msg-page";//will show msg-page.html
+	}
+	
 
 }
