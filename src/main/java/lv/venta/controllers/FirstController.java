1a6cac12ab30ddc564195802fc17f51e5f988e6c
localuser <localuser@vea.lv>
2023-04-21 13:34:00 +0300

    New: controller for product

--- src/main/java/lv/venta/controllers/FirstController.java
+++ src/main/java/lv/venta/controllers/FirstController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lv.venta.model.Product;
+
 @Controller
 public class FirstController {
 	
@@ -23,7 +25,12 @@ public class FirstController {
 		return "msg-page";//will show msg-page.html
 	}
 	
-	//TODO Product class - title, description, price, quantity
 	//TODO controller function which will send new product to frontend
+	@GetMapping("/one-product")//localhost:8080/one-product
+	public String getOneProductFunc(Model model) {
+		Product prod = new Product("Apple", "Tasty", 1.2f, 9);
+		model.addAttribute("packet", prod);
+		return "one-product-page";//will show one-product-page.html
+	}
 
 }
