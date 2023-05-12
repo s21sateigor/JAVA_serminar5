e5f4bc5345aadac2f2db042fdce11d4285acf369
Igors Saters <s21sateigor@venta.lv>
2023-05-12 15:56:17 +0300

    Edit: change service relate to repo

--- src/main/java/lv/venta/repos/IProductRepo.java
+++ src/main/java/lv/venta/repos/IProductRepo.java
@@ -5,4 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface IProductRepo extends CrudRepository<Product, Long> {
 
+    boolean existsByTitleAndDescriptionAndPrice(String inputTitle, String inputDescription, float inputPrice);
+
+    Product findByTitleAndDescriptionAndPrice(String inputTitle, String inputDescription, float inputPrice);
+
 }
--- src/main/java/lv/venta/services/impl/CRUDproductServiceIMPL.java
+++ src/main/java/lv/venta/services/impl/CRUDproductServiceIMPL.java
@@ -3,6 +3,8 @@ package lv.venta.services.impl;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import lv.venta.repos.IProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lv.venta.model.Product;
@@ -11,102 +13,65 @@ import lv.venta.services.iCRUDProductService;
 @Service
 public class CRUDproductServiceIMPL implements iCRUDProductService{
 
-	
-	private ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(
-			new Product("Watermelon", "Pink", 1.23f, 4),
-			new Product("Tomato", "Red", 0.99f, 3),
-			new Product("Grapes", "Purple", 12.3f, 4)
-			));
-	
+	@Autowired
+	private IProductRepo productRepo;
+
+
 	@Override
 	public void addNewProduct(String title, String description, float price, int quantity) throws Exception {
-		//TODO verification with regex title and description
-		if(title!=null && description!=null && price>0 &&
-				price < 10000 && quantity > 0 && quantity < 100000) {
-			boolean isFound = false;
-			for(Product temp: allProducts) {
-				if(temp.getTitle().equals(title) && temp.getDescription().equals(description)
-						&& temp.getPrice()==price) {
-					temp.setQuantity(temp.getQuantity() + quantity);
-					isFound = true;
-					break;
-				}
-			}
-			if(!isFound) {
+
+			if(productRepo.existsByTitleAndDescriptionAndPrice(title, description, price)) {
+				Product temp = productRepo.findByTitleAndDescriptionAndPrice(title, description, price);
+				temp.setQuantity(temp.getQuantity() + quantity);
+				productRepo.save(temp);
+			} else {
 				Product newProduct = new Product(title, description, price, quantity);
-				allProducts.add(newProduct);
+				productRepo.save(newProduct);
 			}
-			
-		}
-		else
-		{
-			throw new Exception("Incorrect params");
-		}
-		
 	}
 
 	@Override
 	public ArrayList<Product> retrieveAllproducts() {
-		return allProducts;
+		return (ArrayList<Product>) productRepo.findAll();
 	}
 
 	@Override
 	public Product retrieveProductsById(long id) throws Exception {
-		if(id > 0) {
-			for(Product temp: allProducts) {
-				if(temp.getId() == id) {
-					return temp;
-				}
+		if (id > 0) {
+
+			if (productRepo.existsById(id)) {
+				Product temp = productRepo.findById(id).get();
+				return temp;
+			} else {
+				throw new Exception("There is no product with this ID");
 			}
-			throw new Exception("There is not product with this ID");
-		}
-		else
-		{
-			throw new Exception("ID need to be positive");
+		} else {
+			throw new Exception("Incorrect ID");
 		}
 	}
 
 	@Override
 	public void updateProductById(long id, String title, String description, float price, int quantity) throws Exception {
 		if(id > 0) {
-			if(title!=null && description!=null && price>0 &&
-					price < 10000 && quantity >= 0 && quantity < 100000) {
-				
-				boolean isFound = false;
-				for(Product temp: allProducts) {
-					if(temp.getId()==id) {
-						temp.setTitle(title);
-						temp.setDescription(description);
-						temp.setPrice(price);
-						temp.setQuantity(quantity);
-						isFound = true;
-						break;
-					}
-				}
-				if(!isFound) {
-					throw new Exception("There is not product with this ID");
-				}
-				
-				
+			if (productRepo.existsById(id)) {
+				Product temp = productRepo.findById(id).get();
+				temp.setTitle(title);
+				temp.setDescription(description);
+				temp.setPrice(price);
+				temp.setQuantity(quantity);
+				productRepo.save(temp);
+			} else {
+				throw new Exception("There is not product with this ID");
 			}
-			else
-			{
-				throw new Exception("Incorrect params");
-			}
-		}
-		else
-		{
-			throw new Exception("ID need to be positive");
 		}
 	}
 
 	@Override
 	public void deleteById(long id) throws Exception {
-		
-		Product deletedProduct = retrieveProductsById(id);
-		allProducts.remove(deletedProduct);
-		
-		
+
+		if(productRepo.existsById(id)){
+			productRepo.deleteById(id);
+		}
 	}
 
 }
